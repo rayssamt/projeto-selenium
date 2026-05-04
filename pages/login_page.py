9fb77f13dd06514fe754f7df84ef74093c7e3c71
rayssamt <rayssamt60@gmail.com>
2026-05-04 10:45:27 -0300

    separando locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,8 +7,11 @@ class LoginPage:
 
     def __init__(self):
         self.driver = conftest.driver
+        self.username_field = (By.ID, "username")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
 
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(By.ID, "username").send_keys(usuario)
-        self.driver.find_element(By.ID, "password").send_keys(senha)
-        self.driver.find_element(By.ID, "login-button").click()
+        self.driver.find_element(*self.username_field).send_keys(usuario)
+        self.driver.find_element(*self.password_field).send_keys(senha)
+        self.driver.find_element(*self.login_button).click()
