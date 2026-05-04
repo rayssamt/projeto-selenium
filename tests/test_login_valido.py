666d2fdcc778b9eab5c0544ebf66b2f2498c8f73
rayssamt <rayssamt60@gmail.com>
2026-05-04 09:48:40 -0300

    conftest

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -1,14 +1,11 @@
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
+from conftest import driver
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://leogcarvalho.github.io/test-automation-practice/")
-
-driver.find_element(By.ID, "username").send_keys("admin")
-driver.find_element(By.ID, "password").send_keys("1234")
-driver.find_element(By.ID, "login-button").click()
-assert driver.find_element(By.ID, "login-status").is_displayed()
-#numa aplicção onde roda o comando várias vezes, add o refresh() no final para a mensagem do 'login-status'desaparecer
-
+@pytest.mark.usefixtures("setup_teardown")
+class TestLogin:
+    def test_login_valido(self):
+        driver.find_element(By.ID, "username").send_keys("admin")
+        driver.find_element(By.ID, "password").send_keys("1234")
+        driver.find_element(By.ID, "login-button").click()
+        assert driver.find_element(By.ID, "login-status").is_displayed()
--- tests/test_select.py
+++ tests/test_select.py
@@ -1,12 +1,11 @@
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from conftest import driver
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://leogcarvalho.github.io/test-automation-practice/index.html")
-
-dropdown = Select(driver.find_element(By.ID, "dropdown"))
-dropdown.select_by_index(1)
-assert driver.find_element(By.XPATH, "//option[@value='option2']").is_selected()
+@pytest.mark.usefixtures("setup_teardown")
+class TestSelect:
+    def test_select_dropdown(self):
+        dropdown = Select(driver.find_element(By.ID, "dropdown"))
+        dropdown.select_by_index(1)
+        assert driver.find_element(By.XPATH, "//option[@value='option2']").is_selected()
