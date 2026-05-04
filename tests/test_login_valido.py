a91d3fba79650a7b4ff4b0a70342810ace6e61a7
rayssamt <rayssamt60@gmail.com>
2026-05-04 10:05:10 -0300

    removendo teste login invalido

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium.webdriver.common.by import By
-from conftest import driver
+import conftest
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestLogin:
     def test_login_valido(self):
+        driver = conftest.driver
         driver.find_element(By.ID, "username").send_keys("admin")
         driver.find_element(By.ID, "password").send_keys("1234")
         driver.find_element(By.ID, "login-button").click()
--- tests/test_select.py
+++ tests/test_select.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from conftest import driver
+import conftest
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestSelect:
     def test_select_dropdown(self):
+        driver = conftest.driver
         dropdown = Select(driver.find_element(By.ID, "dropdown"))
         dropdown.select_by_index(1)
         assert driver.find_element(By.XPATH, "//option[@value='option2']").is_selected()
