34f81f1cfe30b30ed469f3292efd03e9466338d8
rayssamt <rayssamt60@gmail.com>
2026-05-04 10:58:17 -0300

    abstraindo locators da basepage

---
+++ pages/base_page.py
@@ -0,0 +1,18 @@
+import conftest
+
+
+class BasePage:
+    def __init__(self):
+        self.driver = conftest.driver
+
+    def encontrar_elemento(self, locator):
+        return self.driver.find_element(*locator)
+
+    def encontrar_elementos(self, locator):
+        return self.driver.find_elements(*locator)
+
+    def escrever(self, locator, texto):
+        self.encontrar_elemento(locator).send_keys(texto)
+
+    def clicar(self, locator):
+        self.encontrar_elemento(locator).click()
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
-
 import conftest
+from pages.base_page import BasePage
 
 
-class LoginPage:
+class LoginPage(BasePage):
 
     def __init__(self):
         self.driver = conftest.driver
@@ -12,6 +12,6 @@ class LoginPage:
         self.login_button = (By.ID, "login-button")
 
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(*self.username_field).send_keys(usuario)
-        self.driver.find_element(*self.password_field).send_keys(senha)
-        self.driver.find_element(*self.login_button).click()
+        self.escrever(self.username_field, usuario)
+        self.escrever(self.password_field, senha)
+        self.clicar(self.login_button)
